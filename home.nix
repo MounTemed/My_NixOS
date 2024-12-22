31bfb171cc44fd3242734d68f56e1964b388ef1c
LaiMicK <hyprland@vk.com>
2024-12-22 15:59:35 +0500

    remove #

--- home.nix
+++ home.nix
@@ -1,8 +1,4 @@
-{ config, pkgs, inputs, ... }:
-
-{
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
+{ pkgs, inputs, ... }: {
   home.username = "laimick";
   home.homeDirectory = "/home/laimick";
 
@@ -16,20 +12,11 @@
 
   nixpkgs.config.allowUnfree = true;
 
-
- # nix = {
- #   settings = {
- #     download-buffer-size = 268435456; # 256MB
- #   };
- # };
-
   programs.ags = {
      enable = true;
 
-     # symlink to ~/.config/ags
      configDir = ./home/service/ags;
 
-     # additional packages to add to gjs's runtime
      extraPackages = with pkgs; [
        inputs.ags.packages.${pkgs.system}.battery
        fzf
@@ -41,16 +28,7 @@
     inputs.ags.packages.${pkgs.system}.notifd
   ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "24.11";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }

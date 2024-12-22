4c69fbd6c56db2252a661e54743fe6e1a2108641
LaiMicK <hyprland@vk.com>
2024-12-22 19:58:17 +0500

    steam repair

--- home/modules/desktop/games/steam/default.nix
+++ home/modules/desktop/games/steam/default.nix
@@ -1,8 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
+
+  home.packages = with pkgs; [
+    steam
+  ];
+
   programs.steam = {
     enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
   };
 }

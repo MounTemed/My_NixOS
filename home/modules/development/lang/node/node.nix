461ba34d7b2ead11d95ca7a5c6ca081e79090aa9
LaiMicK <hyprland@vk.com>
2024-12-22 19:56:20 +0500

    remove node.nix npm.fuck

--- home/modules/development/lang/node/node.nix
+++ home/modules/development/lang/node/node.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
     nodejs
-  ];
-
-  home.npm.enable = true;
-  home.npm.packages = with pkgs.nodejs.pkgs; [
     nodemon
     eslint
     prettier
